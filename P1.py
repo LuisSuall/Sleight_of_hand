410627c4efd24000a230c345cd902cc3f772dfa5
Griger <gustavo.rg92@gmail.com>
2015-10-19 20:51:07 +0200

    playing with functors

--- P1.py
+++ P1.py
@@ -1,5 +1,21 @@
 import Leap
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
-def main:
-    
+def f():
+    print "I'm f"
+
+class Gesture:
+    def __init__(self, func):
+        self.f = func
+
+    def execute(self):
+        self.f()
+
+def main():
+    a = Gesture(f)
+    a.execute()
+
+    print "End"
+
+if __name__ == '__main__':
+    main()
