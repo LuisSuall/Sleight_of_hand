5ba2803de17dc8a622d353f315372d9f3df3b7db
Griger <gustavo.rg92@gmail.com>
2015-10-30 19:29:02 +0100

    Code commented and other changes.
    We have removed the Photographer class from main.py.
    We have changed the distance in detectOKGesture in gesture.py.

--- Finger_run/main.py
+++ Finger_run/main.py
@@ -3,48 +3,17 @@ from math import *
 import utils.graphics as graphics
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
-class Photographer(Leap.Listener):
-
-    def turnOnCamera():
-        photo_name = raw_input("Insert photo's basis name, please: ")
-
-        print "Press Enter to take photo..."
-        try:
-            sys.stdin.readline()
-        except KeyboardInterrupt:
-            pass
-        finally:
-            takePhoto(photo_name)
-
-    def takePhoto(photo_name):
-        frame = controller.frame()
-        finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
-        bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
-
-        for hand in frame.hands:
-            dataContainer = open(photoName+str(hand.id), 'w')
-
-            print "Storing hand data..."
-
-            for finger in hand.fingers:
-                dataContainer.write("Data from %s \n" %finger_names[finger.type])
-                for b in range(0, 4):
-                    bone = finger.bone(b)
-                    dataContainer.write("Data from %s \n" %bone_names[bone.type])
-                    dataContainer.write("Start: %s \n Final: %s \n" %(bone.prev_joint, bone.next_joint))
-                dataContainer.write("_______________________________________ \n")
-
-            print "Storage complete."
-
 def main(arguments):
-
+	
+	#We set the tolerance to 10% by default.
 	tolerance = 10
 
 	if (len(arguments) == 2):
-		tolerance = int(arguments[1])
-
+		tolerance = int(arguments[1]) #We change the tolerance if we get an argument.
+	
+	#We create a new controller.
 	controller = Leap.Controller()
-
+	
 	graphics.init(arguments, controller, tolerance)
 
 if __name__ == '__main__':
--- Finger_run/utils/gesture.py
+++ Finger_run/utils/gesture.py
@@ -3,18 +3,32 @@ from extendedHand import *
 import math
 from math import *
 
+'''
+Function that calculates the @percent % of @whole
+'''
 def percentage(whole, percent):
 	return (whole * percent) / 100.0
 
+'''
+Function that detects the run gesture.
+@hand: the hand that we're analysing.
+@tolerance: the percentage of tolerance in the measurements.
+'''
 def detectRunGesture(hand, tolerance):
-    index = getFinger(hand, 'index')
-    middle = getFinger(hand, 'middle')
     #We use the index and the middle finger like two legs and we're going to simulate de run action.
     #Then we need the position information about these fingers.
+    index = getFinger(hand, 'index')
+    middle = getFinger(hand, 'middle')
+    
+    #We get the tips position of the two fingers.
     index_tip_pos = index.bone(3).next_joint
     middle_tip_pos = middle.bone(3).next_joint
-
-    diffBtwTipsY = index_tip_pos[1] - middle_tip_pos[1] #We compare the Y coordenates of the tips.
+	 
+	 '''
+	 We calculate the signed difference between the Y coordenates.
+	 We use the sign to check that the fingers have been moved.
+	 '''
+    diffBtwTipsY = index_tip_pos[1] - middle_tip_pos[1]
 
     #We check the palm orientation and we want a minimum distance between the two fingers.
     if detectRunGesture.sign*diffBtwTipsY <= (-30 + percentage(30, tolerance)) and palmOrientation(hand) == 'down':
@@ -25,16 +39,25 @@ def detectRunGesture(hand, tolerance):
 
 detectRunGesture.sign = -1
 
+'''
+Function that detects the OK gesture.
+@hand: the hand that we're analysing.
+@tolerance: the percentage of tolerance in the measurements.
+'''
 def detectOKGesture(hand, tolerance):
-	thumb = getFinger(hand, 'thumb')
+	#We use the index finger and the thumb so we need the position information about these fingers.
+   thumb = getFinger(hand, 'thumb')
 	index = getFinger(hand, 'index')
-
+	
+	#We get the tips position of the two fingers.
 	thumb_tip_pos = thumb.bone(3).next_joint
 	index_tip_pos = index.bone(3).next_joint
 
+	#We calculate the distance between the tips.
 	distanceBtwTips = sqrt(pow(thumb_tip_pos[0]-index_tip_pos[0],2) + pow(thumb_tip_pos[1]-index_tip_pos[1],2) + pow(thumb_tip_pos[2]-index_tip_pos[2],2))
-
-	if distanceBtwTips < (60 + percentage(60, tolerance)) and palmOrientation(hand) == 'down':
+	
+	#We check the palm orientation and the distance between tips.
+	if distanceBtwTips < (50 + percentage(50, tolerance)) and palmOrientation(hand) == 'down':
 		return True
 	else:
 		return False
