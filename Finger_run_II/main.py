c65be6361056b7125d28acbdb1a9182e8ea88f0d
Griger <gustavo.rg92@gmail.com>
2015-11-26 23:52:01 +0100

    Menu commented

--- Finger_run_II/main.py
+++ Finger_run_II/main.py
@@ -8,75 +8,103 @@ from utils.sprite import *
 import game
 import tutorial
 
+'''
+Function that closes the program
+'''
 def close():
 	sys.exit()
 
+'''
+Function that goes to the previous menu
+'''
 def back():
 	global current_menu
 	current_menu.turnOffButtons()
 	current_menu = current_menu.father
 
+'''
+Function that goes to the game menu
+'''
 def goGameMenu():
 	global current_menu, game_menu
 	current_menu.turnOffButtons()
 	current_menu = game_menu
-
+'''
+Function that loads the tutorial
+'''
 def goTutorial():
 	global current_menu
 	current_menu.turnOffButtons()
 	tutorial.main()
-
+	
+'''
+Function that loads the game's level 1
+'''
 def play1():
 	global current_menu
 	current_menu.turnOffButtons()
 	game.main(1)
-
+	
+'''
+Function that loads the game's level 2
+'''
 def play2():
 	global current_menu
 	current_menu.turnOffButtons()
 	game.main(2)
-
+	
+'''
+Function that loads the game's level 3
+'''
 def play3():
 	global current_menu
 	current_menu.turnOffButtons()
 	game.main(3)
 
+'''
+class Menu
+'''
 class Menu:
+	'''
+	@buttons: menu's buttons
+	@name: menu's name
+	@father: previous menu
+	'''
 	def __init__(self, buttons, name, father = None):
 		self.buttons = buttons
 		self.father = father
 		self.name = name
 
+	'''
+	Function that activates all the menu's buttons
+	'''
 	def turnOnButtons(self):
 		for button in self.buttons:
 			button.ON = True
-
+	'''
+	Function that turns off all the menu's buttons
+	'''
 	def turnOffButtons(self):
 		for button in self.buttons:
 			button.ON = False
-
+	
+	'''
+	Function that draws the menu
+	'''
 	def draw(self, surface, frame):
+		#Check OK gesture
 		OK_detected = False
 		for hand in frame.hands:
 			if detectOKGesture(hand, 10):
 				OK_detected = True
-
+				
+		#If no OK gesture then activate menu's buttons
 		if not OK_detected:
 			self.turnOnButtons()
-
+			
+		#Draw menu's buttons
 		for button in self.buttons:
 			button.draw(surface)
-'''
-Function that draws the cursor imagen on the screen
-@cursor: the cursor image
-@frame: the current Leap frame
-'''
-def drawCursor(cursor, frame):
-	for hand in frame.hands:
-		cursor_pos = getTipPosition(hand, 'index')
-		cursor_x = cursor_pos[0]
-		cursor_y = cursor_pos[1]
-		DISPLAYSURF.blit(cursor, (cursor_x+400,-cursor_y+1000))
 
 def main(arguments):
 
@@ -92,21 +120,24 @@ def main(arguments):
 
 	global DISPLAYSURF
 	global current_menu
+	
+	#Configure game screen and its title
 	DISPLAYSURF = pygame.display.set_mode((640,480),0,32)
 	pygame.display.set_caption('Game Screen')
+	
+	#Fill the screen with white
 	DISPLAYSURF.fill((255,255,255))
 
 	#create cursor's sprite
-	#cursor_sprite = Sprite('images/ph_cursor.png',(400,500,43,43))
 	cursor = Cursor('images/ph_cursor.png',(400,500,43,43))
 
 	#we create a new controller
 	controller = Leap.Controller()
-	controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP)
-
+	
 	initial_buttons = []
 	game_buttons = []
-
+	
+	#Create menus' buttons
 	title = Button('images/title.png', (170,25,300,150),"")
 	exit_button = Button('images/button.png',(220,380,200,80),"EXIT",close)
 
@@ -129,33 +160,46 @@ def main(arguments):
 	game_buttons.append(level3_button)
 	game_buttons.append(back_button)
 	#game_buttons.append(exit_button)
-
+	
+	#Create menus
 	global initial_menu, game_menu
 	initial_menu = Menu(initial_buttons, "Initial menu")
 	game_menu = Menu(game_buttons, "Game menu", initial_menu)
-
+	
+	#The first menu is the initial menu
 	current_menu = initial_menu
 
-
+	#Update game screen
 	pygame.display.update()
 	# run the game loop
 
 	while  True:
+		#Fill the screen with white
 		DISPLAYSURF.fill((221,215,153))
+		
+		#Check for QUIT method to close game
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				pygame.quit()
 				sys.exit()
-
+				
+		#Get the new frame
 		frame = controller.frame()
+		
+		#Update cursor data
 		cursor.update(frame)
-
+		
+		#Draw Exit button and current menu buttons
 		exit_button.draw(DISPLAYSURF)
 		current_menu.draw(DISPLAYSURF, frame)
-
+		
+		#Check buttons clicked
 		cursor.collision(current_menu.buttons+[exit_button])
+		
+		#Draw cursor
 		cursor.draw(DISPLAYSURF)
-
+	
+		#Update screen
 		pygame.display.update()
 
 
